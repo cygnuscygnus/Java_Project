c19aacef50d963b813beb63f814481620cfa1bd0
github cygnuscygnus <tonberryapp@gmail.com>
2019-03-06 01:12:13 +0900

    add FizzBuzz

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class FizzBuzz {	
+	public void fizzBuzz(int max_count) {
+		List<String> fbList = new ArrayList<>();
+		for(int i = 1; i <= max_count; i++) {
+		if(i % 15 == 0)
+			fbList.add(i-1,"FizzBuzz");
+		else if(i % 3 == 0 && i % 15 != 0)
+			fbList.add(i-1,"Fizz");
+		else if(i % 5 == 0)
+			fbList.add(i-1,"Buzz");
+		else 
+			fbList.add(i-1,Integer.toString(i));
+		}
+		
+		for(String str : fbList)
+	        System.out.println(str);
+	}
+}
---
+++ FizzBuzz/Sample.java
@@ -0,0 +1,14 @@
+/**
+ * @author tm
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	  FizzBuzz fb = new FizzBuzz();
+	  fb.fizzBuzz(Integer.parseInt(args[0]));
+	 }
+	}
